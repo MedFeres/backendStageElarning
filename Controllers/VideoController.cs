da99580c0e9c7badb6ffb12b2fcb1ff69d9fd3a4
Safa Maalej <safaamaalej@gmail.com>
2025-06-25 14:42:17 +0100

    Fix: ignore Visual Studio temp files

---
+++ Controllers/VideoController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ElearningBackend.Models;
+
+namespace ElearningBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VideoController : ControllerBase
+    {
+        private readonly ElearningDbContext _context;
+
+        public VideoController(ElearningDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Video
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Video>>> GetVideos()
+        {
+            return await _context.Videos.ToListAsync();
+        }
+
+        // GET: api/Video/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Video>> GetVideo(int id)
+        {
+            var video = await _context.Videos.FindAsync(id);
+            if (video == null)
+                return NotFound();
+
+            return video;
+        }
+
+        // POST: api/Video
+        [HttpPost]
+        public async Task<ActionResult<Video>> PostVideo(Video video)
+        {
+            _context.Videos.Add(video);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetVideo), new { id = video.Id }, video);
+        }
+
+        // PUT: api/Video/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVideo(int id, Video video)
+        {
+            if (id != video.Id)
+                return BadRequest();
+
+            _context.Entry(video).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Videos.Any(e => e.Id == id))
+                    return NotFound();
+                else
+                    throw;
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Video/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVideo(int id)
+        {
+            var video = await _context.Videos.FindAsync(id);
+            if (video == null)
+                return NotFound();
+
+            _context.Videos.Remove(video);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Migrations/20250618135217_InitialCreate.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ElearningBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Certificats",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomClient = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateObtention = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Certificats", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Utilisateurs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Prenom = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CompteBancaire = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(13)", maxLength: 13, nullable: false),
+                    EstVerifie = table.Column<bool>(type: "bit", nullable: true),
+                    Cv = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Diplome = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Utilisateurs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cours",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EstPayant = table.Column<bool>(type: "bit", nullable: false),
+                    Prix = table.Column<float>(type: "real", nullable: false),
+                    FormateurId = table.Column<int>(type: "int", nullable: false),
+                    ClientId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cours", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cours_Utilisateurs_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Utilisateurs",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Cours_Utilisateurs_FormateurId",
+                        column: x => x.FormateurId,
+                        principalTable: "Utilisateurs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Paiements",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Montant = table.Column<float>(type: "real", nullable: false),
+                    DatePaiement = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ClientId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Paiements", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Paiements_Utilisateurs_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Utilisateurs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contenus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EstPayant = table.Column<bool>(type: "bit", nullable: false),
+                    Fichier = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CoursId = table.Column<int>(type: "int", nullable: false),
+                    TypeContenu = table.Column<string>(type: "nvarchar(8)", maxLength: 8, nullable: false),
+                    Niveau = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SeuilMinimalEasy = table.Column<int>(type: "int", nullable: true),
+                    SeuilMinimalMedium = table.Column<int>(type: "int", nullable: true),
+                    SeuilMinimalHard = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contenus", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Contenus_Cours_CoursId",
+                        column: x => x.CoursId,
+                        principalTable: "Cours",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contenus_CoursId",
+                table: "Contenus",
+                column: "CoursId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cours_ClientId",
+                table: "Cours",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cours_FormateurId",
+                table: "Cours",
+                column: "FormateurId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Paiements_ClientId",
+                table: "Paiements",
+                column: "ClientId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Certificats");
+
+            migrationBuilder.DropTable(
+                name: "Contenus");
+
+            migrationBuilder.DropTable(
+                name: "Paiements");
+
+            migrationBuilder.DropTable(
+                name: "Cours");
+
+            migrationBuilder.DropTable(
+                name: "Utilisateurs");
+        }
+    }
+}
